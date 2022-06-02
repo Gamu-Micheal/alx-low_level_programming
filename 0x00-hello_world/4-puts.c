e70c6f7451c8af50b401766ba274614da49ba2fe
Gamu Micheal <gamumicheal@gmail.com>
2022-06-02 08:47:38 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *  * main - Entry Point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+
+int main()
+{
+	   	
+	    strcpy(str1, "\"Programming is like building a multilingual puzzle");
+
+	        puts(srt1);
+
+
+		    return(0);
+}
