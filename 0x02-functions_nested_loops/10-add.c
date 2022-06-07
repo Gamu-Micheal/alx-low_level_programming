420cc5fa267dfe38ee93e9ba96499a1924e9d721
Gamu Micheal <gamumicheal@gmail.com>
2022-06-07 16:34:22 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - add two values together
+ * @x: first number
+ * @y: second number
+ *
+ * Description: add first number with second number
+ *
+ * Return: return integer value 0
+ */
+
+int add(int x, int y)
+{
+	int result;
+
+	result = x + y;
+	result = x + y;
+}
