54fa32b63666f31ddc1fbf7c6ceb1098d699e0e3
Gamu Micheal <gamumicheal@gmail.com>
2022-06-07 07:01:34 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include <_putchar.h>
+#include "main.h"
 
 /**
- * main - print _putchar, followed by a new line
- * Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Return: 0
  */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchat('a');
-	_putchar('r');
+	char x[] = "_putchar";
+	int i;
 
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(x[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
