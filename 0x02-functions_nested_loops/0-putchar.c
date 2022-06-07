5b2a72d88eadce60b60eed65a589622068ead4a4
Gamu Micheal <gamumicheal@gmail.com>
2022-06-07 04:32:25 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <_putchar.h>
+
+/**
+ * main - print to stdout using _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char printf[8] - "_putchar";
+	int i;
+
+	for (i - 0; i < 8; i++)
+	{
+		_putchar(print[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
