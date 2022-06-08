8203749dd66ddf4d9efe704d015f22d8ffd72f8a
Gamu Micheal <gamumicheal@gmail.com>
2022-06-07 23:44:14 -0700

    10. a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * add - Check main
+ * add - Add two values
  * @a: An integer a
  * @b: An integer b
  * Description: Function that adds two integers and returns the result
