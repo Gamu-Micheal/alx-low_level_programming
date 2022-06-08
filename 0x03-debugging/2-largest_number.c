560afdaa51a8998ba5f2dfdf9c1554dc28ca9f65
Gamu Micheal <gamumicheal@gmail.com>
2022-06-08 13:43:59 -0700

    2. 0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,19 +10,19 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+int largest;
 
 	if (a >= b && a >= c)
 	{
-		largest = a;
+	largest = a;
 	}
 	else if (b >= a && b >= c)
 	{
-		largest = b;
+	largest = b;
 	}
 	else
 	{
-		largest = c;
+	largest = c;
 	}
 
 	return (largest);
