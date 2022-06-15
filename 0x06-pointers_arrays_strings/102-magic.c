afa9cb56506f9465a690e5b3c7b4d0d4a8f66bf7
Gamu Micheal <gamumicheal@gmail.com>
2022-06-15 16:51:20 -0700

    fixed

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - Add one line to this code
- * so that the program prints a[2] = 98
- *  followed by a new line
- *
- *  Return: Always (0).
- */
-
 int main(void)
 {
 	int n;
